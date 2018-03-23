e663beed530c717bb94bed4139932e05c776a99d
shuaicj <shuaicj@gmail.com>
2018-03-23 14:06:03 +0800

    fix start.sh usage message

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 usage() {
-    echo "USAGE: ./start-all.sh [slave-num]"
+    echo "USAGE: ./start.sh [slave-num]"
     echo "the param [slave-num] is optional, default 3"
 }
 
